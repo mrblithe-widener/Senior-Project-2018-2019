63b334440c5b86c5ca4888c31e504606293e847d
Michael Blithe <michaelrblithe@gmail.com>
2019-04-11 15:38:46 -0400

    Added code to request index and additional info for school display

--- ClientApp/src/components/SchoolDisplay.js
+++ ClientApp/src/components/SchoolDisplay.js
@@ -6,15 +6,54 @@ import SchoolSearchBar from "./SchoolSearchBar";
 
 class SchoolDisplay extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state = {hasRequestedOthers:false};
+    }
+
+    
+
     componentDidMount(){
+        if(this.props.match.params.ncesschid){
+            this.setState({hasRequestedIndex:true});
+            this.props.requestIndex(this.props.match.params.ncesschid);
+        }
         //this.props.requestMath("421476001901");
     }
+
+    componentWillReceiveProps(newProps){
+        console.log(newProps)
+        if(newProps.Index && !this.props.Index){
+            this.setState({hasRequestedOthers:true});
+            this.props.requestMath(newProps.Index.nceesch);
+            this.props.requestRead(newProps.Index.nceesch);
+            if(newProps.Index.zip && newProps.Index.zip !== ''){
+                this.props.requestBusiness(newProps.Index.zip);
+                this.props.requestIncome(newProps.Index.zip);
+            }
+            this.props.requestFunding(newProps.Index.leaid);
+            this.props.requestGeo(newProps.Index.nceesch);
+            this.props.requestTeacherRatios(newProps.Index.nceesch);
+            this.props.requestTitle1(newProps.Index.nceesch);
+        }
+    }
+
     render(){
         return (<div>
-            <SchoolSearchBar />
             {this.props.match.params.ncesschid}
              </div>);
     }
 }
 
-export default connect(() => { return {}}, (dispatch)=>bindActionCreators(actionCreators, dispatch))(SchoolDisplay);
+export default connect((state) => {console.log(state); return {
+        Math_Scores:state.school.Math_Scores, 
+        Read_Scores:state.school.Read_Scores,
+        Business:state.school.Business,
+        Funding:state.school.Funding,
+        Geo:state.school.Geo,
+        Income:state.school.Income,
+        Index:state.school.Index,
+        TeacherRatios:state.school.TeacherRatios,
+        Title1:state.school.Title1
+
+}}, (dispatch)=>bindActionCreators(actionCreators, dispatch))(SchoolDisplay);
