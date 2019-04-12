9fc761320d1dd7acccb2c2be5d74521b2931b57b
Michael Blithe <michaelrblithe@gmail.com>
2019-04-11 21:05:14 -0400

    Added a link to the search bar

--- ClientApp/src/components/SchoolSearchBar.js
+++ ClientApp/src/components/SchoolSearchBar.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { Form, FormGroup, ListGroup, ListGroupItem, Input } from "reactstrap";
 import {actionCreators} from "../store/Search"
+import {Link} from "react-router-dom";
 
 class SchoolSearchBar extends React.Component {
     componentWillMount() {
@@ -21,7 +22,7 @@ class SchoolSearchBar extends React.Component {
             {this.props.searchResults && this.props.searchResults.length > 0 ?
                 <div>
                     <ListGroup>
-                        {this.props.searchResults.map((x, i) => <ListGroupItem key={i}>{`${x.name}, ${x.countyName}, ${x.state}`} </ListGroupItem>)}
+                        {this.props.searchResults.map((x, i) => <ListGroupItem key={i}><Link to={`/school/${x.nceesch}`}>{`${x.name}, ${x.countyName}, ${x.state}`}</Link> </ListGroupItem>)}
                         <ListGroupItem> Can't find what your looking for? Try entering the zip code where the school is located. </ListGroupItem>
                     </ListGroup>
                 </div>
