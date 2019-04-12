8f1292687f877fca9dd94ca0681348ca89828a42
Michael Blithe <michaelrblithe@gmail.com>
2019-04-12 10:36:10 -0400

    Fixed bug with exting search bar.

--- ClientApp/src/components/SchoolSearchBar.js
+++ ClientApp/src/components/SchoolSearchBar.js
@@ -6,8 +6,9 @@ import {actionCreators} from "../store/Search"
 import {Link} from "react-router-dom";
 
 class SchoolSearchBar extends React.Component {
-    componentWillMount() {
-        //this.props.requestSearch("Paxon");
+    constructor(props){
+        super(props);
+        this.state = {hideSearch:true}
     }
     onInputChange(e) {
         this.props.requestSearch(e.currentTarget.value);
@@ -15,17 +16,41 @@ class SchoolSearchBar extends React.Component {
     handleSubmit(e){
         e.preventDefault();
     }
+    searchChildrenTree(current, target){
+        if(current === target)
+            return true;
+        if(!current || current.children.length === 0)
+            return false
+        for(let i = 0; i<current.children.length; i++){
+            if(this.searchChildrenTree(current.children[i], target))
+                return true;
+        }
+        return false;
+    }
+    handleFocusEnter(e){
+        this.setState({hideSearch:false});
+    }
+    handleFocusLeave(e){
+        const parent = document.getElementById("School-Search-Container");
+        const searchResult = this.searchChildrenTree(parent, e.relatedTarget)
+        if(this.searchChildrenTree(parent, e.relatedTarget))//Check if the new target is a child of the search bar container
+            this.setState({hideSearch:false});
+        else 
+            this.setState({hideSearch:true})
+    }
     render() {
-        return (<div>
+        return (<div id="School-Search-Container">
             <Form onSubmit={this.handleSubmit.bind(this)}>
                 <FormGroup>
-                    <Input type="text" autoComplete="off" name="school_name" id="school_name" onChange={this.onInputChange.bind(this)} placeholder="Start typing a school name" />
+                    <Input type="text" autoComplete="off" name="school_name" id="school_name" onChange={this.onInputChange.bind(this)} onFocus={this.handleFocusEnter.bind(this)}  onBlur={this.handleFocusLeave.bind(this)}
+                    placeholder="Start typing a school name" />
                 </FormGroup>
             </Form>
-            {this.props.searchResults && this.props.searchResults.length > 0 ?
-                <div>
+            {!this.state.hideSearch && this.props.searchResults && this.props.searchResults.length > 0 ?
+                <div >
                     <ListGroup>
-                        {this.props.searchResults.map((x, i) => <ListGroupItem key={i}><Link to={`/school/${x.nceesch}`}>{`${x.name}, ${x.countyName}, ${x.state}`}</Link> </ListGroupItem>)}
+                        {this.props.searchResults.map((x, i) => <ListGroupItem onFocus={this.handleFocusEnter.bind(this)} onBlur={this.handleFocusLeave.bind(this)} key={i}>
+                            <Link onFocus={this.handleFocusEnter.bind(this)} onBlur={this.handleFocusLeave.bind(this)} to={`/school/${x.nceesch}`}>{`${x.name}, ${x.countyName}, ${x.state}`}</Link> </ListGroupItem>)}
                         <ListGroupItem> Can't find what your looking for? Try entering the zip code where the school is located. </ListGroupItem>
                     </ListGroup>
                 </div>
